0d72157da404f7fbb85fa85c7ce2a0552ca3fe00
Allen Garvey <allengarvey@hotmail.com>
2019-06-17 22:52:17 -0400

    Change default app ports to be sequential numbers

--- apps/photog/config/dev.exs
+++ apps/photog/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :photog, PhotogWeb.Endpoint,
-  http: [port: 5897],
+  http: [port: 6014],
   debug_errors: true,
   code_reloader: true,
   check_origin: false
--- apps/seren/config/dev.exs
+++ apps/seren/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :seren, SerenWeb.Endpoint,
-  http: [port: 6099],
+  http: [port: 6013],
   debug_errors: true,
   code_reloader: true,
   check_origin: false#,
