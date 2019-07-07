0f5b85a2a47f5db2a6fae0da9010ce9c5b527480
Allen Garvey <allengarvey@hotmail.com>
2019-07-07 16:43:12 -0400

    Rename artour entry js files to match convention of other apps

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -26,8 +26,8 @@ module.exports = {
         'bookmarker': `${__dirname}/../apps/bookmarker/assets/js/index.js`,
         'movielist': `${__dirname}/../apps/movielist/assets/js/app.js`,
         'blockquote': `${__dirname}/../apps/blockquote/assets/sass/admin.scss`,
-        'artour_admin': `${__dirname}/../apps/artour/assets/js/admin/admin.js`,
-        'artour_public': `${__dirname}/../apps/artour/assets/js/public/app.js`,
+        'artour_admin': `${__dirname}/../apps/artour/assets/js/admin/index.js`,
+        'artour_public': `${__dirname}/../apps/artour/assets/js/public/index.js`,
     },
     output: {
         path: path.join(__dirname, '..', 'apps'),
