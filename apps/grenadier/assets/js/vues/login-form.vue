702785bd7cce76765a688f4f41072710a83f932d
Allen Garvey <allengarvey@hotmail.com>
2019-07-14 23:21:56 -0400

    Change password input type to password

--- apps/grenadier/assets/js/vues/login-form.vue
+++ apps/grenadier/assets/js/vues/login-form.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="login-form">
         <input type="text" placeholder="Identifier"/>
-        <input type="text" placeholder="Proof"/>
+        <input type="password" placeholder="Proof"/>
         <button>Login</button>
     </div>
 </template>
