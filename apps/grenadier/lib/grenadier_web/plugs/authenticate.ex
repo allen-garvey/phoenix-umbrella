71df1895b6cc0f255bcc93c7d47c21357f71f264
Allen Garvey <allengarvey@hotmail.com>
2019-07-18 23:09:34 -0400

    Disable caching in authentication plug to prevent using back button after logging out

--- apps/grenadier/lib/grenadier_web/plugs/authenticate.ex
+++ apps/grenadier/lib/grenadier_web/plugs/authenticate.ex
@@ -13,14 +13,26 @@ defmodule GrenadierWeb.Plugs.Authenticate do
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
     case user_id && Account.get_user(user_id) do
-      %User{} = user -> assign(conn, :current_user, user)
+      %User{} = user -> disable_caching(conn) |> assign(:current_user, user)
       nil ->
         # IO.inspect conn
         # TODO change to redirect to remove subdomain from host and replace with grenadier
         # and add grenadier login path user Routes
         conn
+        |> disable_caching()
         |> Phoenix.Controller.redirect(external: "http://" <> conn.host)
         |> halt()
     end
+  end
+
+  @doc """
+  Disables caching for authenticated requests so can't use back button after logging out
+  based on: https://stackoverflow.com/questions/33554022/prevent-user-from-accessing-previous-page-using-back-button-after-logout
+  """
+  def disable_caching(conn) do
+    conn
+    |> put_resp_header("cache-control", "no-cache, no-store, must-revalidate")
+    |> put_resp_header("pragma", "no-cache")
+    |> put_resp_header("expires", "0")
   end
 end
