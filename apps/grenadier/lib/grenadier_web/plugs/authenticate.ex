36aa4c1d0ad45a76a8748b6b60db4d2adc543fe1
Allen Garvey <allen.garvey@gmail.com>
2019-07-19 23:48:52 -0400

    Update grenadier authentication plug to use correct scheme, port and path for login redirect

--- apps/grenadier/lib/grenadier_web/plugs/authenticate.ex
+++ apps/grenadier/lib/grenadier_web/plugs/authenticate.ex
@@ -4,6 +4,8 @@ defmodule GrenadierWeb.Plugs.Authenticate do
   alias Grenadier.Account
   alias Grenadier.Account.User
 
+  alias GrenadierWeb.Router.Helpers, as: Routes
+
   # based on authentication plug from programming phoenix
 
   def init(opts) do
@@ -20,7 +22,7 @@ defmodule GrenadierWeb.Plugs.Authenticate do
         # and add grenadier login path user Routes
         conn
         |> disable_caching()
-        |> Phoenix.Controller.redirect(external: "http://" <> conn.host)
+        |> Phoenix.Controller.redirect(external: get_failed_login_redirect_url(conn))
         |> halt()
     end
   end
@@ -34,5 +36,33 @@ defmodule GrenadierWeb.Plugs.Authenticate do
     |> put_resp_header("cache-control", "no-cache, no-store, must-revalidate")
     |> put_resp_header("pragma", "no-cache")
     |> put_resp_header("expires", "0")
+  end
+
+  @doc """
+  Gets url to redirect to after failed login
+  """
+  def get_failed_login_redirect_url(conn) do
+    "#{get_failed_login_redirect_scheme(conn.scheme)}://#{conn.host}#{get_failed_login_redirect_port(conn.port)}#{Routes.page_path(conn, :login)}"
+  end
+
+  @doc """
+  Gets the port to redirect to in url after failed login
+  """
+  def get_failed_login_redirect_port(origin_port) do
+    if origin_port == 80 or origin_port == 443 do
+      ""
+    else
+      ":#{origin_port}"
+    end
+  end
+
+  @doc """
+  Gets the scheme to redirect to in url after failed login
+  """
+  def get_failed_login_redirect_scheme(origin_scheme) do
+    case origin_scheme do
+      :https -> "https"
+      _      -> "http"
+    end
   end
 end
