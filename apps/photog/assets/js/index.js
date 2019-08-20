5878debce8505ce541f4f925308b34e062436f34
Allen Garvey <allengarvey@hotmail.com>
2019-08-19 22:49:53 -0400

    Refactor photog app vue instantiation to use shared function

--- apps/photog/assets/js/index.js
+++ apps/photog/assets/js/index.js
@@ -2,25 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './components/app.vue'
 import routes from './routes.js'
+import { instantiateVue } from '../../../common/assets/js/vue/instantiate-vue.js'
 
 import css from '../sass/style.scss';
 
 Vue.use(VueRouter);
 const router = new VueRouter(routes);
 
-(function(){
-    const appContainer = document.getElementById('app');
-
-    if(appContainer){
-        const csrfToken = appContainer.dataset.csrfToken;
-
-        new Vue({
-            el: appContainer,
-            render: h => h(App, {props: {csrfToken}}),
-            router,
-        });
-    }
-})();
+instantiateVue('app', App, ['csrfToken'], {router});
 
 
 
