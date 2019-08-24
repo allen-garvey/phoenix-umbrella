707554f5c33532ad1d31c6af0319629495c883a9
Allen Garvey <allengarvey@hotmail.com>
2019-08-24 01:07:29 -0400

    Create shared js module with ajax functions

---
+++ apps/common/assets/js/ajax.js
@@ -0,0 +1,22 @@
+/*
+ * Common functions to fetch and send json to phoenix api 
+ */
+
+export function fetchJson(url){
+    return fetch(url)
+        .then(data => data.json())
+        .then(json => json.data);
+}
+
+export function sendJson(url, csrfToken='', method='POST', data={}){
+    return fetch(url, 
+        {
+            method,
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-Token': csrfToken,
+            },
+
+        }).then(data=>data.json());
+}
