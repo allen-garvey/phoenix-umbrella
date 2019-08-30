fcb18e3207cbbbc37f91553bcf73fc68d6b61204
Allen Garvey <allengarvey@hotmail.com>
2019-08-29 22:11:21 -0400

    Update webpack config for sass-loader 8

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -46,7 +46,9 @@ module.exports = {
                     {
                         loader: 'sass-loader',
                         options: {
-                            outputStyle: 'compressed',
+                            sassOptions: {
+                                outputStyle: 'compressed',
+                            },
                         },
                     },
                 ]
