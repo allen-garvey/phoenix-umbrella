d1f75f6b2e0ea5f767693199e9a2bb3756be6385
Allen Garvey <allengarvey@hotmail.com>
2019-10-14 22:53:13 -0400

    Automatically create grenadier login resource when user attempts to log in

--- apps/grenadier/lib/grenadier_web/controllers/page_controller.ex
+++ apps/grenadier/lib/grenadier_web/controllers/page_controller.ex
@@ -24,10 +24,13 @@ defmodule GrenadierWeb.PageController do
   def login_submit(conn, %{"username" => username, "password" => password}) do
     case Account.authenticate_user(username, password) do
       {:ok, %User{} = user} -> conn
+                      |> generate_login_resource(username, true)
                       |> put_session(:user_id, user.id)
                       |> configure_session(renew: true)
                       |> redirect_after_login()
-      _   -> login_failed(conn)
+      _   -> conn
+              |> generate_login_resource(username, false)
+              |> login_failed()
     end
   end
 
@@ -47,6 +50,28 @@ defmodule GrenadierWeb.PageController do
       original_request_url ->
         delete_session(conn, :original_request_url)
         |> redirect(external: original_request_url)
+    end
+  end
+
+  @doc """
+  Creates login resource to keep record of login attempts
+  """
+  def generate_login_resource(conn, username, was_successful) do
+    [user_agent] = get_req_header(conn, "user-agent")
+    remote_ip = conn.remote_ip
+                  |> Tuple.to_list
+                  |> Enum.join(".")
+    login_params = %{
+      username: username,
+      was_successful: was_successful,
+      ip: remote_ip,
+      user_agent: user_agent,
+    }
+    # don't check if creation fails or not, since we don't want errors
+    # saving login attempt to prevent users from logging in
+    case Account.create_login(login_params) do
+      _ ->
+        conn
     end
   end
 end
