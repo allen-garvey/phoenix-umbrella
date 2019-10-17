5754560831bf184795cd1578aca316998ef63b11
Allen Garvey <allengarvey@hotmail.com>
2019-10-16 23:34:58 -0400

    Add styles to login index table in grenadier app to highlight unsuccessful logins

--- apps/grenadier/lib/grenadier_web/views/login_view.ex
+++ apps/grenadier/lib/grenadier_web/views/login_view.ex
@@ -1,3 +1,11 @@
 defmodule GrenadierWeb.LoginView do
   use GrenadierWeb, :view
+
+  def login_row_class(login_was_successful) when is_boolean(login_was_successful) do
+    case login_was_successful do
+      true  -> ""
+      false -> "login-failed"
+    end
+  end
+
 end
