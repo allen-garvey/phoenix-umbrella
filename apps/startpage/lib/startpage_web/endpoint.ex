c0e92ac49df7aa0ec347340cc6ff0449dc58b86e
Allen Garvey <allengarvey@hotmail.com>
2019-12-11 14:42:27 -0500

    Fix startpage app not using common session options

--- apps/startpage/lib/startpage_web/endpoint.ex
+++ apps/startpage/lib/startpage_web/endpoint.ex
@@ -36,10 +36,7 @@ defmodule StartpageWeb.Endpoint do
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_startpage_key",
-    signing_salt: "sF++TgEs"
+  plug Plug.Session, Common.Endpoint.session_options()
 
   plug StartpageWeb.Router
 end
