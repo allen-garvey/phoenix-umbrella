a5c05494ef00f693fbfa3ed682edcebb970fe20e
Allen Garvey <allengarvey@hotmail.com>
2019-12-12 12:49:39 -0500

    Add js for startpage index page

--- apps/startpage/assets/js/public/index.js
+++ apps/startpage/assets/js/public/index.js
@@ -2,3 +2,16 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import css from "../../css/public.scss"
+
+document.querySelectorAll('.menu_container .menu_title').forEach((caller)=>{
+    caller.addEventListener('click', ()=>{
+        const menuContainer = caller.parentElement;
+        menuContainer.classList.toggle('active');
+
+        //scroll to start of contents on mobile
+        if(window.innerWidth <= 600 && menuContainer.classList.contains('active')){
+            const menuContents = menuContainer.querySelector('.menu_contents');
+            window.scrollTo(0, menuContents.offsetTop);
+        }
+    }); 
+});
