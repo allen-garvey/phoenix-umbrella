6e074e0fa3659f418b682202e624a1ce31b86033
Allen Garvey <allengarvey@hotmail.com>
2019-12-25 17:43:55 -0500

    Update functions to add amazon photos ids to images to try matching just on filename first before trying with the date

--- apps/photog/lib/photog/shutterbug/shutterbug.ex
+++ apps/photog/lib/photog/shutterbug/shutterbug.ex
@@ -24,17 +24,56 @@ defmodule Photog.Shutterbug do
   def add_amazon_photos_id(amazon_photos_id, master_file_name, creation_year, creation_month, creation_day)
     when is_binary(amazon_photos_id) and is_binary(master_file_name) and is_integer(creation_year) and is_integer(creation_month) do
       file_name_regex = "/#{master_file_name}$"
-      now = DateTime.utc_now()
 
+      case add_amazon_photos_id_helper(amazon_photos_id, file_name_regex) do
+        {:ok, _} -> true
+        {_, _} -> add_amazon_photos_id_helper(amazon_photos_id, file_name_regex, creation_year, creation_month, creation_day)
+      end
+  end
+
+  @doc """
+  First try adding amazon photos id based on filename, but if adds to more than 1
+  rollback and try to be more specific by using dates
+  """
+  def add_amazon_photos_id_helper(amazon_photos_id, file_name_regex) do
+    now = DateTime.utc_now()
+
+    try do
       Repo.transaction(fn ->
-        from(
+        case from(
           i in Image,
-          where: fragment("? ~ ?", i.master_path, ^file_name_regex) and is_nil(i.amazon_photos_id) and fragment("EXTRACT(year FROM ?)", i.creation_time) == ^creation_year and fragment("EXTRACT(month FROM ?)", i.creation_time) == ^creation_month and fragment("EXTRACT(day FROM ?)", i.creation_time) == ^creation_day
+          where: fragment("? ~ ?", i.master_path, ^file_name_regex) and is_nil(i.amazon_photos_id)
         )
         |> Repo.update_all(set: [
           amazon_photos_id: amazon_photos_id,
           updated_at: now,
-        ])
+        ]) do
+          {0, _} -> {:ok, nil}
+          {1, _} -> {:ok, nil}
+          {_, _} -> Repo.rollback(:too_many_updates)
+        end
       end)
+    rescue
+      _ -> {:error, nil}
+    end
+  end
+
+  def add_amazon_photos_id_helper(amazon_photos_id, file_name_regex, creation_year, creation_month, creation_day) do
+    now = DateTime.utc_now()
+
+    Repo.transaction(fn ->
+      try do
+        from(
+        i in Image,
+        where: fragment("? ~ ?", i.master_path, ^file_name_regex) and is_nil(i.amazon_photos_id) and fragment("EXTRACT(year FROM ?)", i.creation_time) == ^creation_year and fragment("EXTRACT(month FROM ?)", i.creation_time) == ^creation_month and fragment("EXTRACT(day FROM ?)", i.creation_time) == ^creation_day
+      )
+      |> Repo.update_all(set: [
+        amazon_photos_id: amazon_photos_id,
+        updated_at: now,
+      ])
+      rescue
+        _ -> Repo.rollback(:too_many_updates)
+      end
+    end)
   end
 end
