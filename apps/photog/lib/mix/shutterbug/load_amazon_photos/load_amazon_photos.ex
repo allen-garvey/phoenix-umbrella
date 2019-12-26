f256e3e1044118b2743fc7e563eb43fef8abea82
Allen Garvey <allengarvey@hotmail.com>
2019-12-25 19:55:46 -0500

    Refactor task to get amazon photos id to take directory of import

--- apps/photog/lib/mix/shutterbug/load_amazon_photos/load_amazon_photos.ex
+++ apps/photog/lib/mix/shutterbug/load_amazon_photos/load_amazon_photos.ex
@@ -11,15 +11,15 @@ defmodule Mix.Tasks.Shutterbug.LoadAmazonPhotos do
   @doc """
   Imports images from source directory to (optional) destination directory. If no directory given, will default to ./priv/static/media, or separate masters and thumbnail destination directories can be given
   """
-  def run([json_file_path]) do
+  def run([json_file_path, folder_name]) do
     case File.read(json_file_path) do
-      {:ok, data} -> Jason.decode!(data) |> add_amazon_photo_ids()
+      {:ok, data} -> Jason.decode!(data) |> add_amazon_photo_ids(folder_name)
       {:error, reason} -> file_read_error(reason, json_file_path)
     end
   end
 
   def run(_args) do
-  	Error.exit_with_error("usage: mix shutterbug.load_amazon_photos <json_file_path>")
+  	Error.exit_with_error("usage: mix shutterbug.load_amazon_photos <json_file_path> <import_folder_name>")
   end
 
   def file_read_error(reason, file_path) do
@@ -30,7 +30,7 @@ defmodule Mix.Tasks.Shutterbug.LoadAmazonPhotos do
   @doc """
   Adds Amazon Photos ids to image resources
   """
-  def add_amazon_photo_ids(api_json) do
+  def add_amazon_photo_ids(api_json, folder_name) do
     # disable logging of database queries
     Logger.configure(level: :error)
     #start app so repo is available
@@ -40,7 +40,7 @@ defmodule Mix.Tasks.Shutterbug.LoadAmazonPhotos do
 
     for image_json <- api_json["data"] do
       amazon_id = image_json["id"]
-      masters_filename = image_json["name"]
+      masters_filename = "#{folder_name}/#{image_json["name"]}"
       created_date = cond do
         !is_nil(image_json["contentProperties"]["contentDate"]) -> image_json["contentProperties"]["contentDate"]
         !is_nil(image_json["clientProperties"]["dateCreated"]) -> image_json["clientProperties"]["dateCreated"]
