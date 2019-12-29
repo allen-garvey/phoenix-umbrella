827f1ba4e11b43d14ba0bf2b003e47310d6855d3
Allen Garvey <allengarvey@hotmail.com>
2019-12-29 10:58:34 -0500

    For movielist reports don't show next year link when showing reports for current year, and redirect to current year if request for future year

--- apps/movielist/lib/movielist_web/controllers/reports_controller.ex
+++ apps/movielist/lib/movielist_web/controllers/reports_controller.ex
@@ -2,35 +2,47 @@ defmodule MovielistWeb.ReportsController do
   use MovielistWeb, :controller
 
   alias Movielist.Reports
+  alias MovielistWeb.ReportsView
 
   def report_for_year(conn, year, sort) when is_integer(year) and is_atom(sort) do
-    rating_stats = Reports.rating_stats_for_year(year)
-    ratings = Reports.list_ratings_for_year(year, sort)
-    ratings_count_by_month = Reports.get_ratings_count_by_month(year, year == Date.utc_today.year)
+    current_year = Date.utc_today.year
 
-    render(conn, "show.html", 
-      page_atom: :reports_show,
-      year: year,
-      ratings: ratings,
-      rating_count: rating_stats[:rating_count], 
-      average_score: rating_stats[:average_score],
-      ratings_count_by_month: ratings_count_by_month
-    )
+    if year > current_year do
+      invalid_year_redirect(conn)
+    else
+      rating_stats = Reports.rating_stats_for_year(year)
+      ratings = Reports.list_ratings_for_year(year, sort)
+      ratings_count_by_month = Reports.get_ratings_count_by_month(year, year == current_year)
+      should_show_next_year = year < current_year
+
+      render(conn, "show.html",
+        page_atom: :reports_show,
+        year: year,
+        ratings: ratings,
+        rating_count: rating_stats[:rating_count],
+        average_score: rating_stats[:average_score],
+        ratings_count_by_month: ratings_count_by_month,
+        should_show_next_year: should_show_next_year,
+      )
+    end
   end
 
   def show(conn, %{"year" => year_raw, "sort" => "date"}) do
     case Integer.parse(year_raw) do
       {year, _} -> report_for_year(conn, year, :date)
-      _         -> redirect(conn, to: MovielistWeb.ReportsView.reports_for_current_year_date_sorted_path(conn))
+      _         -> invalid_year_redirect(conn)
     end
   end
 
   def show(conn, %{"year" => year_raw}) do
     case Integer.parse(year_raw) do
       {year, _} -> report_for_year(conn, year, :score)
-      _         -> redirect(conn, to: MovielistWeb.ReportsView.reports_for_current_year_score_sorted_path(conn))
+      _         -> invalid_year_redirect(conn)
     end
   end
 
-  
+  def invalid_year_redirect(conn) do
+    redirect(conn, to: ReportsView.reports_for_current_year_score_sorted_path(conn))
+  end
+
 end
