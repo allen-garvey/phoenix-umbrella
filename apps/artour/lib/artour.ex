57fa48f09bec25209b159fad753a4675dd84273c
Allen Garvey <allengarvey@hotmail.com>
2020-05-08 20:14:48 -0400

    Update artour project structure to match other projects

--- apps/artour/lib/artour.ex
+++ apps/artour/lib/artour.ex
@@ -1,31 +1,9 @@
 defmodule Artour do
-  use Application
+  @moduledoc """
+  Artour keeps the contexts that define your domain
+  and business logic.
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    import Supervisor.Spec
-
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Start the Ecto repository
-      supervisor(Artour.Repo, []),
-      # Start the endpoint when the application starts
-      supervisor(Artour.Endpoint, []),
-      # Start your own worker by calling: Artour.Worker.start_link(arg1, arg2, arg3)
-      # worker(Artour.Worker, [arg1, arg2, arg3]),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Artour.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
-
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
-  def config_change(changed, _new, removed) do
-    Artour.Endpoint.config_change(changed, removed)
-    :ok
-  end
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
 end
--- apps/artour/mix.exs
+++ apps/artour/mix.exs
@@ -23,7 +23,7 @@ defmodule Artour.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [
-      mod: {Artour, []},
+      mod: {Artour.Application, []},
       extra_applications: [:logger, :runtime_tools]
    ]
   end
