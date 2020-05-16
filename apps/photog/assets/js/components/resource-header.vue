d210f69b6bfdaa3fc3aa3682b854a924024fe8d8
Allen Garvey <allengarvey@hotmail.com>
2020-05-15 20:11:57 -0400

    Convert photog resource header component styles to css module

--- apps/photog/assets/js/components/resource-header.vue
+++ apps/photog/assets/js/components/resource-header.vue
@@ -1,8 +1,21 @@
 <template>
-    <div class="spread-content resource-header-container">
+    <div 
+        class="spread-content"
+        :class="$style['resource-header-container']"
+    >
         <div>
-            <h1 class="resource-header-title" v-if="title">{{title}}</h1>
-            <p v-if="description" class="resource-header-description">{{description}}</p>
+            <h1 
+                :class="$style['resource-header-title']" 
+                v-if="title"
+            >
+                {{title}}
+            </h1>
+            <p 
+                v-if="description" 
+                :class="$style['resource-header-description']"
+            >
+                {{description}}
+            </p>
         </div>
         <div class="pull-right" v-if="newItemLink || editItemLink">
             <router-link :to="newItemLink" class="btn btn-success" v-if="newItemLink">New</router-link>
@@ -10,6 +23,22 @@
         </div>
     </div>
 </template>
+
+<style lang="scss" module>
+    .resource-header-container{
+        margin-bottom: 0.5em;
+    }
+
+    .resource-header-title{
+        margin: 0;
+    }
+
+    .resource-header-description{
+        color: #777;
+        margin: 0;
+        font-size: 0.95em;
+    }
+</style>
 
 <script>
 export default {
@@ -28,17 +57,5 @@ export default {
             type: Object,
         },
     },
-    components: {
-    },
-    created(){
-    },
-    data() {
-        return {
-        }
-    },
-    computed: {
-    },
-    methods: {
-    }
 }
 </script>
