681fb856064824698b3eb9d586fc1a1ec387a2d8
Allen Garvey <allengarvey@hotmail.com>
2020-05-19 22:41:13 -0400

    Convert photog imports index styles to css module

--- apps/photog/assets/js/components/imports-index.vue
+++ apps/photog/assets/js/components/imports-index.vue
@@ -1,9 +1,9 @@
 <template>
     <Text-List :title="pageTitle" :items-list="itemsList" :is-initial-load-complete="isInitialLoadComplete" :load-more-items-callback="loadMoreItems">
         <template v-slot:item="{item, index}">
-            <router-link :to="showRouteFor(item)" class="imports-index-item-link">
+            <router-link :to="showRouteFor(item)">
                 <div>{{titleFor(item)}}</div>
-                <ul class="imports-index-mini-thumbnail-list">
+                <ul :class="$style['imports-index-mini-thumbnail-list']">
                     <li v-for="image in item.images" :key="image.id">
                         <img :src="thumbnailUrlFor(image)"/>
                     </li>
@@ -12,6 +12,27 @@
         </template>
     </Text-List>
 </template>
+
+<style lang="scss" module>
+    .imports-index-mini-thumbnail-list{
+        display: inline-flex;
+        flex-wrap: wrap;
+        img{
+            object-fit: cover;
+            object-position: center;
+            height: 130px;
+            width: 140px;
+            transition: width 0.4s, height 0.75s;
+        }
+
+        &:hover{
+            img{
+                height: 240px;
+                width: 280px;
+            }
+        }
+    }
+</style>
 
 <script>
 import { thumbnailUrlFor } from '../image.js';
