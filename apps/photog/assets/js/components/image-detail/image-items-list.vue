7c172b366557db84e29e6fe6237e7f29506fa578
Allen Garvey <allengarvey@hotmail.com>
2020-05-21 21:12:35 -0400

    Move photog image items list component styles to css module

--- apps/photog/assets/js/components/image-detail/image-items-list.vue
+++ apps/photog/assets/js/components/image-detail/image-items-list.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="image-show-text-list-container">
-        <div class="image-show-text-list-heading">
-            <h3 class="image-show-text-list-title">{{heading}}</h3>
-            <div class="button-container">
+    <div :class="$style['image-show-text-list-container']">
+        <div :class="$style['image-show-text-list-heading']">
+            <h3 :class="$style['image-show-text-list-title']">{{heading}}</h3>
+            <div :class="$style['button-container']">
                 <button :disabled="isAddMode" @click="editItemsButtonAction" class="btn btn-sm" :class="{'btn-secondary': isEditMode, 'btn-outline-secondary': !isEditMode}" v-if="hasItems && !isAddMode">{{editButtonText}}</button>
                 <button :disabled="isEditMode" @click="addItemsButtonAction" class="btn btn-sm" :class="addButtonCssClass" v-if="!isEditMode || !hasItems">{{addButtonText}}</button>
                 <button :disabled="!areAnyItemsToBeAddedSelected" v-if="isAddMode" @click="saveAddItems" class="btn btn-sm btn-success">Save</button>
             </div>
         </div>
         <div v-if="isAddMode">
-            <ul class="image-show-add-items-list">
+            <ul :class="$style['image-show-add-items-list']">
                 <li v-for="(item, index) in itemsThatCanBeAdded" :key="index">
                     <input type="checkbox" :id="idForItemToBeAdded(item, index)" v-model="itemsThatCanBeAddedSelected[index]" />
                     <label :for="idForItemToBeAdded(item, index)">{{item.name}}</label>
                 </li>
             </ul>
         </div>
-        <ul class="image-show-text-list" v-if="hasItems">
+        <ul :class="$style['image-show-text-list']" v-if="hasItems">
             <li v-for="(item, index) in items" :key="item.id">
                 <router-link :to="{name: itemRouteName, params: {id: item.id}}">{{item.name}}</router-link>
                 <button :style="{visibility: isEditMode ? 'visible' : 'hidden'}" @click="deleteItem(item, index)" class="btn btn-xs btn-outline-danger">Delete</button>
@@ -24,6 +24,47 @@
         </ul>
     </div>
 </template>
+
+<style lang="scss" module>
+    .image-show-text-list-container{
+        margin-top: 3.5em;
+    }
+    .image-show-text-list-heading{
+        display: flex;
+        flex-wrap: wrap;
+        margin-bottom: 0.5em;
+
+        .button-container{
+            margin-left: 2em;
+        }
+    }
+    .image-show-text-list{
+        padding-left: 1em;
+        li{
+            margin-bottom: 0.5em;
+            display: flex;
+            flex-wrap: wrap;
+
+            & > *:first-child{
+                flex-basis: 25%;
+            }
+        }
+    }
+    .image-show-text-list-title{
+        margin: 0;
+        align-self: flex-end;
+    }
+    .image-show-add-items-list{
+        margin-top: 1em;
+        display: flex;
+        flex-wrap: wrap;
+
+        li{
+            margin-bottom: 0.5em;
+            flex-basis: 25%;
+        }
+    }
+</style>
 
 <script>
 import { fetchJson } from 'umbrella-common-js/ajax.js';
