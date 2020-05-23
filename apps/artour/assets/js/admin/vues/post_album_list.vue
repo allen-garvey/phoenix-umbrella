cefd2a036378be41fd77c86866f71547b241ed50
Allen Garvey <allengarvey@hotmail.com>
2020-05-23 13:03:55 -0400

    Move artour admin post show page component styles to css module

--- apps/artour/assets/js/admin/vues/post_album_list.vue
+++ apps/artour/assets/js/admin/vues/post_album_list.vue
@@ -1,27 +1,95 @@
 <template>
     <div>
-        <div class="post-album-image-list-controls" :class="{'list-group-item-info': haveImagesBeenReordered}">
-            <button class="btn btn-primary" @click="saveImageOrder()" :style="{visibility: haveImagesBeenReordered ? 'visible' : 'hidden'}">Save Image Order</button>
+        <div :class="controlsClasses">
+            <button 
+                class="btn btn-primary" 
+                @click="saveImageOrder()" 
+                :style="{visibility: haveImagesBeenReordered ? 'visible' : 'hidden'}"
+            >
+                Save Image Order
+            </button>
         </div>
-        <ol class="post-album-image-list">
-            <li v-for="(postImage, index) in postImages" :key="postImage.id" draggable="true" :class="{'cover-image-container': postImage.image.id === coverImageId}" @dragstart="imageDragStart($event, index)" @dragover="imageDraggedOver($event, index)" @drop="imageDropped($event)">
+        <ol :class="$style['post-album-image-list']">
+            <li 
+                v-for="(postImage, index) in postImages" 
+                :key="postImage.id" 
+                draggable="true" 
+                :class="{[$style['cover-image-container']]: postImage.image.id === coverImageId}" 
+                @dragstart="imageDragStart($event, index)" 
+                @dragover="imageDraggedOver($event, index)" 
+                @drop="imageDropped($event)"
+            >
                 <div>
-                    <a :href="postImage.image.url.self"><img :src="postImage.image.url.thumbnail" :alt="postImage.image.description"/></a>
+                    <a :href="postImage.image.url.self">
+                        <img :src="postImage.image.url.thumbnail" :alt="postImage.image.description"/>
+                    </a>
                 </div>
-                <div class="image-title">
+                <div :class="$style['image-title']">
                     <a :href="postImage.image.url.self">{{postImage.image.title}}</a>
                     <p v-if="postImage.caption">{{postImage.caption}}</p>
                 </div>
-                <div class="image-buttons">
+                <div :class="$style['image-buttons']">
                     <a :href="postImage.url.edit" class="btn btn-default" target="_blank">Edit</a>
                     <button class=" btn btn-primary" v-show="postImage.image.id != coverImageId" @click="setCoverImage(postImage.image.id)">Make cover image</button>
                 </div>
-                <div class="list-item-dragger">&#9776;</div>
+                <div :class="$style['list-item-dragger']">&#9776;</div>
             </li>
         </ol>
-
     </div>
 </template>
+
+<style lang="scss" module>
+    @import '~artour-styles/admin/variables';
+
+    .post-album-image-list-controls{
+        display: flex;
+        justify-content: flex-end;
+        padding: 1em;
+        border-radius: 3px;
+        margin-top: 3em;
+    }
+
+    .post-album-image-list{
+        padding: 0;
+        list-style-type: none;
+        margin-top: 10px;
+        img{
+            height: 114px;
+        }
+        li{
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            border-bottom: 1px solid #ddd;
+            &:last-of-type{
+                border-bottom: none;
+            }
+            & > *{
+                display: flex;
+                align-items: center;
+            }
+            .image-title{
+                width: 20em;
+                flex-direction: column;
+                justify-content: center;
+            }
+            .list-item-dragger{
+                padding: 0 2em;
+                font-size: 2em;
+                cursor: move;
+            }
+        }
+        .cover-image-container{
+            background-color: $item_selected_color;
+        }
+        .image-buttons > *{
+            margin-right: 1em;
+            &:last-child{
+                margin-right: 0;
+            }
+        } 
+    }
+</style>
 
 <script>
 import Vue from 'vue';
@@ -61,9 +129,12 @@ export default {
         };
     },
     computed: {
-
-    },
-    watch: {
+        controlsClasses(){
+            return {
+                [this.$style['post-album-image-list-controls']]: true,
+                'list-group-item-info': this.haveImagesBeenReordered,
+            };
+        },
     },
     methods: {
         fetchImages(){
