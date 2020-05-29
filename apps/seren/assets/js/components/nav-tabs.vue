c29b7bcb3c505f6ec465d667d3c4639cb3def847
Allen Garvey <allengarvey@hotmail.com>
2020-05-29 09:01:00 -0400

    Seren: move nav tab styles into css module

--- apps/seren/assets/js/components/nav-tabs.vue
+++ apps/seren/assets/js/components/nav-tabs.vue
@@ -1,12 +1,52 @@
 <template>
-    <nav class="nav">
-        <ul class="nav-list nav-pills">
+    <nav :class="$style['nav']">
+        <ul 
+            :class="[$style['nav-list'], $style['nav-pills']]"
+        >
             <li v-for="(tab, i) in tabs" :key="i">
-                <router-link :to="tab.route(searchQuery)">{{ tab.title }}</router-link>
+                <router-link 
+                    :to="tab.route(searchQuery)"
+                    :active-class="$style['router-link-active']"
+                >
+                    {{ tab.title }}
+                </router-link>
             </li>
         </ul>
     </nav>
 </template>
+
+<style lang="scss" module>
+    @import '~seren-styles/variables';
+
+    .nav{
+        font-family: sans-serif;
+    }
+
+    .nav-list{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        padding: 0;
+        list-style-type: none;
+    }
+
+    .nav-pills li a{
+        text-decoration: none;
+        color: $accent_color_text;
+        cursor: pointer;
+        padding: 0.5em 1em;
+
+        &:hover{
+            color: #0056b3;
+        }
+
+        &.router-link-active{
+            background: $accent_color_text;
+            color: white;
+            border-radius: 4px;	
+        }
+    }
+</style>
 
 <script>
 import Models from '../models';
@@ -26,7 +66,5 @@ export default {
 	},
 	computed: {
 	},
-	methods: {
-	}
 };
 </script>
