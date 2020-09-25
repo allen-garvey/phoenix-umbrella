a524aa1f6a040bb652ada77129a1394737457ff2
Allen Garvey <allengarvey@hotmail.com>
2020-09-25 19:59:52 -0400

    Refactor artour to use common function to instantiate vue

--- apps/artour/assets/js/admin/index.js
+++ apps/artour/assets/js/admin/index.js
@@ -9,28 +9,28 @@ initializeFormDeleteButton();
 import { initializeAutofillSlug } from './slugify.js';
 initializeAutofillSlug();
 
-import { initializeVueComponent } from './vue-helpers.js';
+import { instantiateVue } from 'umbrella-common-js/vue/instantiate-vue.js';
 
 import PostAddImages from './vues/post_add_images.vue';
 (function(){
     const keys = ['csrfToken', 'formUrl', 'imagesApiUrl'];
-    initializeVueComponent('post_add_images_component', PostAddImages, keys);
+    instantiateVue('post_add_images_component', PostAddImages, keys);
 })();
 
 import PostAlbumList from './vues/post_album_list.vue';
 (function(){
     const keys = ['csrfToken', 'coverImageId', 'postImagesApiUrl', 'editPostApiUrl', 'reorderImagesApiUrl'];
-    initializeVueComponent('post_album_list_component', PostAlbumList, keys);
+    instantiateVue('post_album_list_component', PostAlbumList, keys);
 })();
 
 import PostAddTagsList from './vues/post_add_tags_list.vue';
 (function(){
     const keys = ['csrfToken', 'postId', 'apiBaseUrl', 'newTagUrl'];
-    initializeVueComponent('post_add_tags_list', PostAddTagsList, keys);
+    instantiateVue('post_add_tags_list', PostAddTagsList, keys);
 })();
 
 import ImportImages from './vues/import_images.vue';
 (function(){
     const keys = ['csrfToken', 'apiFormatIndexUrl', 'apiCreateImagesUrl', 'imagesIndexUrl'];
-    initializeVueComponent('import_images', ImportImages, keys);
+    instantiateVue('import_images', ImportImages, keys);
 })();
