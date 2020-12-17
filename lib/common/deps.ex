e6dde851a24f7463b866c051226e157f2f96fb62
Allen Garvey <allengarvey@hotmail.com>
2020-12-17 08:57:22 -0500

    Update hex dependencies

--- lib/common/deps.ex
+++ lib/common/deps.ex
@@ -2,15 +2,15 @@ defmodule Umbrella.Common.Deps do
   # Shared dependencies for phoenix projects
   def shared_phoenix_deps do
     [
-      {:phoenix, "~> 1.5.4"},
+      {:phoenix, "~> 1.5.7"},
       {:phoenix_pubsub, "~> 2.0"},
       ecto(),
-      {:ecto_sql, "~> 3.4.5"},
+      {:ecto_sql, "~> 3.5.3"},
       {:postgrex, "~> 0.15"},
       {:phoenix_html, "~> 2.14"},
-      {:phoenix_live_reload, "~> 1.2.4", only: :dev},
+      {:phoenix_live_reload, "~> 1.3.0", only: :dev},
       {:gettext, "~> 0.17.1"},
-      {:jason, "~> 1.2"},
+      {:jason, "~> 1.2.2"},
       {:plug_cowboy, "~> 2.4.0"},
       {:common, in_umbrella: true},
       {:assertions, "~> 0.15", only: :test},
@@ -25,7 +25,7 @@ defmodule Umbrella.Common.Deps do
   end
 
   def ecto do
-    {:phoenix_ecto, "~> 4.2"}
+    {:phoenix_ecto, "~> 4.2.1"}
   end
 
   def http_poison do
@@ -33,7 +33,7 @@ defmodule Umbrella.Common.Deps do
   end
 
   def floki do
-    {:floki, "~> 0.28.0"}
+    {:floki, "~> 0.29.0"}
   end
 
   def earmark do
