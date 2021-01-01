e8fddfeb1637bb6e486fe34bc2a56f494b4a1624
Allen Garvey <allengarvey@hotmail.com>
2020-12-31 21:15:01 -0500

    Separate functions in datehelpers to get today's date and time now with timezone offset

--- apps/common/lib/model_helpers/date.ex
+++ apps/common/lib/model_helpers/date.ex
@@ -2,16 +2,29 @@ defmodule Common.ModelHelpers.Date do
   alias Ecto.Changeset
 
   @doc """
+  Gets current datetime, adjusted for timezone
+  """
+  def now() do
+    DateTime.utc_now 
+      # EST UTC offset
+      |> DateTime.add(-5 * 3600, :second)
+  end
+
+  @doc """
+  Gets today's date, adjusted for timezone
+  """
+  def today() do
+    now()
+      |> DateTime.to_date
+  end
+
+  @doc """
   Adds default date as today if nil
   """
   def default_date_today(changeset, attribute_key) do
     date_value = Changeset.get_field(changeset, attribute_key)
     if is_nil(date_value) do
-      date_now = DateTime.utc_now 
-        # EST UTC offset
-        |> DateTime.add(-5 * 3600, :second)
-        |> DateTime.to_date
-      Changeset.change(changeset, %{attribute_key => date_now})
+      Changeset.change(changeset, %{attribute_key => today()})
     else
       changeset
     end
