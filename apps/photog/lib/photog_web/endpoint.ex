edf7c001eb73afae7f00b1ee9f6bc6fa0e79ceae
Allen Garvey <allengarvey@hotmail.com>
2021-04-10 15:08:25 -0400

    Disable js console websocket errors for photog app

--- apps/photog/lib/photog_web/endpoint.ex
+++ apps/photog/lib/photog_web/endpoint.ex
@@ -11,11 +11,11 @@ defmodule PhotogWeb.Endpoint do
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
-  if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
-  end
+  # if code_reloading? do
+  #   socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+  #   plug Phoenix.LiveReloader
+  #   plug Phoenix.CodeReloader
+  # end
 
   plug Plug.Logger
 
