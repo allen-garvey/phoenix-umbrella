72d3d453a680061dc43c731c68c9c3e3317eaeb0
Allen Garvey <allengarvey@hotmail.com>
2021-08-21 09:01:32 -0400

    Update hex dependencies

--- lib/common/deps.ex
+++ lib/common/deps.ex
@@ -2,18 +2,18 @@ defmodule Umbrella.Common.Deps do
   # Shared dependencies for phoenix projects
   def shared_phoenix_deps do
     [
-      {:phoenix, "~> 1.5.8"},
+      {:phoenix, "~> 1.5.11"},
       {:phoenix_pubsub, "~> 2.0"},
       ecto(),
-      {:ecto_sql, "~> 3.5.4"},
-      {:postgrex, "~> 0.15"},
-      {:phoenix_html, "~> 2.14"},
-      {:phoenix_live_reload, "~> 1.3.0", only: :dev},
+      {:ecto_sql, "~> 3.7.0"},
+      {:postgrex, "~> 0.15.10"},
+      {:phoenix_html, "~> 3.0.2"},
+      {:phoenix_live_reload, "~> 1.3.3", only: :dev},
       {:gettext, "~> 0.17.1"},
       {:jason, "~> 1.2.2"},
-      {:plug_cowboy, "~> 2.4.0"},
+      {:plug_cowboy, "~> 2.5.1"},
       {:common, in_umbrella: true},
-      {:assertions, "~> 0.15", only: :test},
+      {:assertions, "~> 0.18.1", only: :test},
     ]
   end
 
@@ -25,7 +25,7 @@ defmodule Umbrella.Common.Deps do
   end
 
   def ecto do
-    {:phoenix_ecto, "~> 4.2.1"}
+    {:phoenix_ecto, "~> 4.4.0"}
   end
 
   def http_poison do
@@ -33,11 +33,11 @@ defmodule Umbrella.Common.Deps do
   end
 
   def floki do
-    {:floki, "~> 0.30.0"}
+    {:floki, "~> 0.31.0"}
   end
 
   def earmark do
-    {:earmark, "~> 1.4.9"}
+    {:earmark, "~> 1.4.15"}
   end
 
   def argon2 do
