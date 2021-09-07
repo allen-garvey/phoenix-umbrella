b56cff14c55d2bb4d0f8cba288a42009014ba5a4
Allen Garvey <allengarvey@hotmail.com>
2021-09-07 19:38:15 -0400

    Change unreleased movie search query from dvd to streaming release date

--- apps/movielist/lib/movielist_web/views/movie_view.ex
+++ apps/movielist/lib/movielist_web/views/movie_view.ex
@@ -48,7 +48,7 @@ defmodule MovielistWeb.MovieView do
   end
 
   def search_query_for_release_status(movie_title, :theater_released, nil) do
-    "#{movie_title} dvd release date"
+    "#{movie_title} streaming release date"
   end
 
   def search_query_for_release_status(movie_title, _release_status, _home_release_date) do
