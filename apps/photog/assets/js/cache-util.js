af179bdf44ba83e645f068e7c2d8dfc93a172292
Allen Garvey <allengarvey@hotmail.com>
2021-12-12 09:11:52 -0500

    Update photog cache util pagination support to support urls with query params

--- apps/photog/assets/js/cache-util.js
+++ apps/photog/assets/js/cache-util.js
@@ -1,8 +1,11 @@
 import { fetchJson } from 'umbrella-common-js/ajax.js';
 
-function fetchIntoCache(url, cacheMap, mapId, options={}){
+function fetchIntoCache(apiUrl, cacheMap, mapId, options={}){
     if(options.isPaginated){
-        const paginatedUrl = `${url}?offset=${options.offset}&limit=${options.limit}`;
+        const url = new URL(apiUrl, window.location);
+        const href = url.href.replace(/\?$/, '');
+        const query = url.search ? url.search + '&' : '?';
+        const paginatedUrl = `${href}${query}offset=${options.offset}&limit=${options.limit}`;
         return fetchJson(paginatedUrl).then((data)=>{
             const oldData = cacheMap.has(mapId) ? cacheMap.get(mapId) : [];
             const combinedData = oldData.concat(data);
@@ -14,7 +17,7 @@ function fetchIntoCache(url, cacheMap, mapId, options={}){
     if(!options.forceRefresh && cacheMap.has(mapId)){
         return Promise.resolve(cacheMap.get(mapId));
     }
-    return fetchJson(url).then((data)=>{
+    return fetchJson(apiUrl).then((data)=>{
         cacheMap.set(mapId, data);
         return data;
     });
