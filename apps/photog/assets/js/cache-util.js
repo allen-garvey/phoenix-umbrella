ce61cc4d81154a80b280712f8ad43b57f568fc02
Allen Garvey <allengarvey@hotmail.com>
2021-12-13 14:15:37 -0500

    Fix photog cache util not properly supporting query params for pagination

--- apps/photog/assets/js/cache-util.js
+++ apps/photog/assets/js/cache-util.js
@@ -3,9 +3,8 @@ import { fetchJson } from 'umbrella-common-js/ajax.js';
 function fetchIntoCache(apiUrl, cacheMap, mapId, options={}){
     if(options.isPaginated){
         const url = new URL(apiUrl, window.location);
-        const href = url.href.replace(/\?$/, '');
         const query = url.search ? url.search + '&' : '?';
-        const paginatedUrl = `${href}${query}offset=${options.offset}&limit=${options.limit}`;
+        const paginatedUrl = `${url.pathname}${query}offset=${options.offset}&limit=${options.limit}`;
         return fetchJson(paginatedUrl).then((data)=>{
             const oldData = cacheMap.has(mapId) ? cacheMap.get(mapId) : [];
             const combinedData = oldData.concat(data);
