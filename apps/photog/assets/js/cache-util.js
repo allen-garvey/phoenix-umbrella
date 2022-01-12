06893dc134bfdeca1ec08de9f0e9d730aaa002c7
Allen Garvey <allengarvey@hotmail.com>
2022-01-12 07:53:31 -0500

    Fix unnecessary api requests when model has been requested before and count is below thumbnail chunk length

--- apps/photog/assets/js/cache-util.js
+++ apps/photog/assets/js/cache-util.js
@@ -1,32 +1,33 @@
 import { fetchJson } from 'umbrella-common-js/ajax.js';
 
 function fetchIntoCache(apiUrl, cacheMap, mapId, options={}){
-    const isCached = !options.forceRefresh && cacheMap.has(mapId);
+    let isCached = !options.forceRefresh && cacheMap.has(mapId);
 
     if(options.isPaginated){
+        const cachedLength = isCached ? cacheMap.get(mapId).offset + cacheMap.get(mapId).limit : 0;
         const desiredLength = options.offset + options.limit;
         
-        if(isCached && cacheMap.get(mapId).length >= desiredLength){
-            return Promise.resolve(cacheMap.get(mapId).slice(0, desiredLength));
+        if(cachedLength >= desiredLength){
+            return Promise.resolve(cacheMap.get(mapId).data.slice(0, desiredLength));
         }
 
         const url = new URL(apiUrl, window.location);
         const query = url.search ? url.search + '&' : '?';
         const paginatedUrl = `${url.pathname}${query}offset=${options.offset}&limit=${options.limit}`;
         return fetchJson(paginatedUrl).then((data)=>{
-            const oldData = cacheMap.has(mapId) ? cacheMap.get(mapId) : [];
+            const oldData = cacheMap.has(mapId) ? cacheMap.get(mapId).data : [];
             const combinedData = oldData.concat(data);
-            cacheMap.set(mapId, combinedData);
+            cacheMap.set(mapId, {data: combinedData, offset: options.offset, limit: options.limit});
             return combinedData;
         });
     }
 
     if(isCached){
-        return Promise.resolve(cacheMap.get(mapId));
+        return Promise.resolve(cacheMap.get(mapId).data);
     }
 
     return fetchJson(apiUrl).then((data)=>{
-        cacheMap.set(mapId, data);
+        cacheMap.set(mapId, {data});
         return data;
     });
 }
