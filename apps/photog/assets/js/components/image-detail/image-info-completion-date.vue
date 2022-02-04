148fcfb5cd76b8aacaf4952d83cb9345bcf60cc6
Allen Garvey <allengarvey@hotmail.com>
2022-02-04 13:23:40 -0500

    Move image completion date form into separate component

---
+++ apps/photog/assets/js/components/image-detail/image-info-completion-date.vue
@@ -0,0 +1,90 @@
+<template>
+    <dt :class="$style.label">Completion Date</dt>
+    <dd>
+        <div v-show="!isEditingCompletionDate">{{formatIsoDate(image.completion_date)}}</div>
+        <div 
+            v-if="isEditingCompletionDate" 
+            :class="$style.editContainer"
+        >
+            <div class="form-group">
+                <input type="date" class="form-control" v-model="completionDateModel"/>
+            </div>
+            <div>
+                <button @click="cancelEditCompletionDate()" class="btn btn-outline-secondary btn">Cancel</button>
+                <button @click="updateImageCompletionDate()" class="btn btn-success btn">Save</button>
+            </div>
+        </div>
+        <div v-show="!isEditingCompletionDate">
+            <button @click="enableEditCompletionDate()" class="btn btn-outline-primary btn-xs">Edit</button>
+        </div>
+    </dd>
+</template>
+
+<style lang="scss" module>
+.label{
+    margin-bottom: 1.5em;
+}
+.editContainer{
+    display: flex;
+    .form-control{
+        max-width: 14em;
+    }
+    .form-group{
+        margin-right: 1rem;
+    }
+}
+</style>
+
+<script>
+import { isoFormattedDateToUs } from '../../date-helpers';
+
+export default {
+    props: {
+        image: {
+            type: Object,
+            required: true,
+        },
+        updateImage: {
+            type: Function,
+            required: true,
+        },
+    },
+    data(){
+        return {
+            completionDateModel: null,
+            isEditingCompletionDate: false,
+        };
+    },
+    computed: {
+    },
+    watch: {
+        image(){
+            this.isEditingCompletionDate = false;
+            this.completionDateModel = this.image.completion_date;
+        },
+    },
+    methods: {
+        formatIsoDate(date){
+            return isoFormattedDateToUs(date);
+        },
+        updateImageCompletionDate(){
+            const data = {
+                image: {
+                    id: this.image.id,
+                    completion_date: this.completionDateModel,
+                },
+            };
+            this.updateImage(data).then((response)=>{
+                this.image.completion_date = response.data.completion_date;
+                this.isEditingCompletionDate = false;
+            });
+        },
+        enableEditCompletionDate(){
+            this.isEditingCompletionDate = true;
+        },
+        cancelEditCompletionDate(){
+            this.isEditingCompletionDate = false;
+        },
+    }
+};
+</script>
--- apps/photog/assets/js/components/image-detail/image-info.vue
+++ apps/photog/assets/js/components/image-detail/image-info.vue
@@ -17,23 +17,10 @@
             </dd>
             <dt>Date Taken</dt>
             <dd>{{image.creation_time.formatted.us_date}} {{image.creation_time.formatted.time}}</dd>
-            <dt :class="$style['image-info-completion-date']">Completion Date</dt>
-            <dd>
-                <div v-show="!isEditingCompletionDate">{{formatIsoDate(image.completion_date)}}</div>
-                <div 
-                    v-if="isEditingCompletionDate" 
-                    :class="$style['image-info-edit-container']"
-                >
-                    <div class="form-group"><input type="date" class="form-control" v-model="completionDateModel"/></div>
-                    <div>
-                        <button @click="cancelEditCompletionDate()" class="btn btn-outline-secondary btn-sm">Cancel</button>
-                        <button @click="updateImageCompletionDate()" class="btn btn-success btn-sm">Save</button>
-                    </div>
-                </div>
-                <div v-show="!isEditingCompletionDate">
-                    <button @click="enableEditCompletionDate()" class="btn btn-outline-primary btn-sm">Edit</button>
-                </div>
-            </dd>
+            <completion-date 
+                :image="image"
+                :update-image="updateImage"
+            />
             <dt>Favorite</dt>
             <dd>{{image.is_favorite ? 'true' : 'false'}}</dd>
             <template v-if="image.import">
@@ -55,24 +42,13 @@
 </template>
 
 <style lang="scss" module>
-    .image-info-completion-date{
-		margin-bottom: 1.5em;
-    }
-    .image-info-edit-container{
-		display: flex;
-		.form-control{
-			max-width: 14em;
-		}
-		.form-group{
-			margin-right: 1rem;
-		}
-	}
+
 </style>
 
 <script>
 import ImageInfoSection from './image-info-section.vue';
 import ImagePath from './image-path.vue';
-import { isoFormattedDateToUs } from '../../date-helpers';
+import CompletionDate from './image-info-completion-date.vue';
 
 export default {
     props: {
@@ -88,43 +64,11 @@ export default {
     components: {
         ImageInfoSection,
         ImagePath,
-    },
-    data(){
-        return {
-            completionDateModel: null,
-            isEditingCompletionDate: false,
-        };
+        CompletionDate,
     },
     computed: {
     },
-    watch: {
-        image(){
-            this.isEditingCompletionDate = false;
-            this.completionDateModel = this.image.completion_date;
-        },
-    },
     methods: {
-        formatIsoDate(date){
-            return isoFormattedDateToUs(date);
-        },
-        updateImageCompletionDate(){
-            const data = {
-                image: {
-                    id: this.image.id,
-                    completion_date: this.completionDateModel,
-                },
-            };
-            this.updateImage(data).then((response)=>{
-                this.image.completion_date = response.data.completion_date;
-                this.isEditingCompletionDate = false;
-            });
-        },
-        enableEditCompletionDate(){
-            this.isEditingCompletionDate = true;
-        },
-        cancelEditCompletionDate(){
-            this.isEditingCompletionDate = false;
-        },
     }
 };
 </script>
