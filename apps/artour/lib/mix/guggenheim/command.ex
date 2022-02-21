982e1f9150731a5c5c2d24ce3c768eeb834c81b5
Allen Garvey <allengarvey@hotmail.com>
2022-02-21 10:19:20 -0500

    Add test for optipng command

--- apps/artour/lib/mix/guggenheim/command.ex
+++ apps/artour/lib/mix/guggenheim/command.ex
@@ -17,12 +17,20 @@ defmodule Artour.Guggenheim.Command do
     def are_import_commands_available do
       image_magick_command = "convert"
       exiftool_command = "exiftool"
+      optipng_command = "optipng"
+      
       if !is_command_available(image_magick_command)  do
         Error.exit_with_error("Image magick '#{image_magick_command}' command is not installed or unavailable", :image_magick_not_available)
       end
-      if !is_command_available(image_magick_command) do
+      
+      if !is_command_available(exiftool_command) do
         Error.exit_with_error("'#{exiftool_command}' command is not installed or unavailable", :exiftool_not_available)
       end
+
+      if !is_command_available(optipng_command) do
+        Error.exit_with_error("'#{optipng_command}' command is not installed or unavailable", :optipng_not_available)
+      end
+
       true
     end
   
