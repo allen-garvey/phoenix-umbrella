ada714188d370e0350cb925232affb21e20ff9c1
Allen Garvey <allengarvey@hotmail.com>
2022-02-22 07:57:03 -0500

    Update guggenheim to insert images in database

--- apps/artour/lib/mix/guggenheim/guggenheim.ex
+++ apps/artour/lib/mix/guggenheim/guggenheim.ex
@@ -33,29 +33,34 @@ defmodule Mix.Tasks.Guggenheim do
       Image.create_liquid_thumbnails(temp_dir, source_image_models)
 
       # Start Artour app so db is available
-      # Wrap for loop in transaction
-      # Create image resource for each image path
-      for {image_path, image_orientation} <- source_image_models do
-        image_title = Image.path_to_title(image_path)
-        
-        image_map = %{
-          "title" => image_title,
-          "description" => image_title,
-          "year" => image_year,
-        }
-        |> Map.merge(Image.generate_images(image_path, temp_dir, image_orientation))
-        
-        IO.inspect image_map
-      end
+      Mix.Task.run "app.start", []
 
-      # Fix permissions on temp folder
+      Artour.Repo.transaction(fn ->
+        # Create image resource for each image path
+        for {image_path, image_orientation} <- source_image_models do
+          IO.puts "Converting and importing #{image_path}"
+          
+          image_title = Image.path_to_title(image_path)
+          
+          image_map = %{
+            "title" => image_title,
+            "description" => image_title,
+            "year" => image_year,
+          }
+          |> Map.merge(Image.generate_images(image_path, temp_dir, image_orientation))
+          
+          {:ok, _image} = Artour.Admin.create_image(image_map)
+        end
+      end, timeout: :infinity)
+
+      # Fix permissions and optimize pngs/jpgs
       Filesystem.fix_permissions(temp_dir)
-      # Run optipng on temp folder
       Image.optimize_pngs(temp_dir)
-      # Run jpg optimization on temp folder
       Image.optimize_jpgs(temp_dir)
-      # Print message to copy temp dir to source image folders
-      IO.puts "Images imported. Copy contents of #{temp_dir} to image source directory"
+      
+      image_priv_dir = Path.join([__ENV__.file, "..", "..", "..", "..", "priv", "static", "media", "images"])
+      |> Path.expand
+      IO.puts "\n\nImages imported. Copy contents of #{temp_dir} to #{image_priv_dir}"
     end
     
     def run(_args) do
