e29d191cdec366420d3ae5be346a99ff50bf193f
Allen Garvey <allengarvey@hotmail.com>
2022-10-18 01:29:00 -0400

    Add plugin count and total cost to pluginista reports page

--- apps/pluginista/lib/pluginista_web/controllers/report_controller.ex
+++ apps/pluginista/lib/pluginista_web/controllers/report_controller.ex
@@ -3,7 +3,10 @@ defmodule PluginistaWeb.ReportController do
   
     def index(conn, _params) do
         years = Pluginista.Reports.years_summary()
-        render(conn, "index.html", years: years)
+        plugins_count = years |> Enum.reduce(0, fn year, total -> year[:count] + total end)
+        total_cost = years |> Enum.reduce(Decimal.new(0), fn year, total -> Decimal.add(total, year[:total]) end)
+        
+        render(conn, "index.html", years: years, plugins_count: plugins_count, total_cost: total_cost)
     end
 end
   
