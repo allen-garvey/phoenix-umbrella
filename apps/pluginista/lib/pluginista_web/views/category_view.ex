24f0b1b7feaebab11fa75c97d4390f5620fd33a2
Allen Garvey <allengarvey@hotmail.com>
2022-10-21 14:33:18 -0400

    Add colors to pluginista plugins table

--- apps/pluginista/lib/pluginista_web/views/category_view.ex
+++ apps/pluginista/lib/pluginista_web/views/category_view.ex
@@ -16,4 +16,30 @@ defmodule PluginistaWeb.CategoryView do
     |> Enum.map(&%{:id => &1.id, :name => &1.name, :group_id => &1.group_id})
     |> Jason.encode!
   end
+
+  @doc """
+  Gets CSS class for category for plugin table
+  """
+  def category_class(nil) do
+    ""
+  end
+  
+  def category_class(category) do
+    colors = [
+      "blue",
+      "teal",
+      "magenta",
+      "yellow",
+      "orange",
+      "green",
+      "red",
+      "cyan",
+      "lime",
+      "bordeaux",
+      "violet",
+      "black",
+    ]
+    
+    "row-#{Enum.at(colors, rem(category.id, Enum.count(colors)))}"
+  end
 end
