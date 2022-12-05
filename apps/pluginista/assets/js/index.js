deefd3efe6620f40266c20bb32cd1074cb2ca4b9
Allen Garvey <allengarvey@hotmail.com>
2022-12-05 07:56:08 -0500

    Add ability to delete resources to pluginista

--- apps/pluginista/assets/js/index.js
+++ apps/pluginista/assets/js/index.js
@@ -5,3 +5,12 @@ import PlugCategories from './vue/plugin-categories.vue';
 import css from '../css/app.scss';
 
 instantiateVue('plugin-categories', PlugCategories, ['categories']);
+
+document.querySelectorAll('[data-button="delete"]').forEach(button => {
+    button.onclick = (e) => {
+        if(!confirm('Are you sure you want to delete?')){
+            e.preventDefault();
+            return false;
+        }
+    };
+});
