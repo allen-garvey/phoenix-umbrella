8dbd06d49976bdae843f9f5be8b64b9513c87ac7
Allen Garvey <allengarvey@hotmail.com>
2022-12-06 09:30:15 -0500

    Habits: Add initial styles for category colors

--- apps/habits/lib/habits_web/views/activity_view.ex
+++ apps/habits/lib/habits_web/views/activity_view.ex
@@ -1,3 +1,4 @@
 defmodule HabitsWeb.ActivityView do
+  alias HabitsWeb.CategoryView
   use HabitsWeb, :view
 end
--- apps/habits/lib/habits_web/views/category_view.ex
+++ apps/habits/lib/habits_web/views/category_view.ex
@@ -1,5 +1,17 @@
 defmodule HabitsWeb.CategoryView do
   use HabitsWeb, :view
+  alias Habits.Admin.Category
 
   Common.ViewHelpers.Form.define_map_for_form(true)
+
+  def color_style(%Category{} = category) do
+    prefix = "category-color category-color--"
+
+    case category.color do
+      "green" -> "#{prefix}green"
+      "black" -> "#{prefix}black"
+      "red" -> "#{prefix}red"
+      _ -> ""
+    end
+  end
 end
