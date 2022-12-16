ea5d0ec772e85b6c1f8b57f6e50a698793884d18
Allen Garvey <allengarvey@hotmail.com>
2022-12-16 14:56:44 -0500

    Habits: Fix exception due to categories map being empty

--- apps/habits/assets/js/vue/activity-calendar.vue
+++ apps/habits/assets/js/vue/activity-calendar.vue
@@ -24,7 +24,7 @@
                 :start-date="currentMonthActivities.meta.from"
                 :end-date="currentMonthActivities.meta.to"
                 :todays-date="formatDate(todaysDate)"
-                v-if="categoriesMap"
+                v-if="categoriesMap.size > 0"
             />
         </div>
     </div>
