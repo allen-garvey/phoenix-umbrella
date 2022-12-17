9fab26ecf905e5a4f959dc77c2f5f2d01ea5bb85
Allen Garvey <allengarvey@hotmail.com>
2022-12-17 11:20:25 -0500

    Habits: Add ability to delete resources

--- apps/habits/assets/js/index.js
+++ apps/habits/assets/js/index.js
@@ -5,3 +5,12 @@ import ActivityCalendar from './vue/activity-calendar.vue';
 import css from '../css/app.scss';
 
 instantiateVue('activity-calendar', ActivityCalendar);
+
+document.querySelectorAll('[data-button="delete"]').forEach(button => {
+    button.onclick = (e) => {
+        if(!confirm('Are you sure you want to delete?')){
+            e.preventDefault();
+            return false;
+        }
+    };
+});
