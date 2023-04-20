882cc63d13c4f9b5772430ff0a0cf6c825d32b88
Allen Garvey <allengarvey@hotmail.com>
2023-04-20 09:14:12 -0400

    Pluginista: Fix plugins api route to return list instead of map

--- apps/pluginista/lib/pluginista_web/views/api_plugin_view.ex
+++ apps/pluginista/lib/pluginista_web/views/api_plugin_view.ex
@@ -3,20 +3,17 @@ defmodule PluginistaWeb.ApiPluginView do
 
     def render("index.json", %{conn: conn, plugins: plugins}) do
         %{
-            data: Map.new(plugins, fn plugin -> 
-                {
-                    plugin.id,
-                    %{
-                        id: plugin.id,
-                        name: plugin.name,
-                        cost: plugin.cost,
-                        acquisition_date: plugin.acquisition_date,
-                        group_id: plugin.group_id,
-                        maker_id: plugin.maker_id,
-                        category_ids: Enum.map(plugin.plugin_categories, fn plugin_category -> plugin_category.category_id end),
-                        url: Routes.plugin_path(conn, :show, plugin),
-                        edit_url: Routes.plugin_path(conn, :edit, plugin),
-                    }
+            data: Enum.map(plugins, fn plugin -> 
+                %{
+                    id: plugin.id,
+                    name: plugin.name,
+                    cost: plugin.cost,
+                    acquisition_date: plugin.acquisition_date,
+                    group_id: plugin.group_id,
+                    maker_id: plugin.maker_id,
+                    category_ids: Enum.map(plugin.plugin_categories, fn plugin_category -> plugin_category.category_id end),
+                    url: Routes.plugin_path(conn, :show, plugin),
+                    edit_url: Routes.plugin_path(conn, :edit, plugin),
                 }
             end)
         }
