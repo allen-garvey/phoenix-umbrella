cc139c2db686f19360e12f6de8460f5f11f4fb22
Allen Garvey <allengarvey@hotmail.com>
2023-04-22 07:40:38 -0400

    Pluginista: Add sorting capability to vue plugin table

--- apps/pluginista/assets/js/vue/plugin-table.vue
+++ apps/pluginista/assets/js/vue/plugin-table.vue
@@ -5,18 +5,18 @@
         <thead>
             <tr>
                 <th></th>
-                <th>Maker</th>
-                <th>Name</th>
-                <th>Group</th>
-                <th>Category</th>
-                <th>Acquisition date</th>
-                <th>Cost</th>
+                <th><a href="" @click="updateSort($event, sortColumns.maker)">Maker</a></th>
+                <th><a href="" @click="updateSort($event, sortColumns.name)">Name</a></th>
+                <th><a href="" @click="updateSort($event, sortColumns.group)">Group</a></th>
+                <th><a href="" @click="updateSort($event, sortColumns.category)">Category</a></th>
+                <th><a href="" @click="updateSort($event, sortColumns.date)">Acquisition date</a></th>
+                <th><a href="" @click="updateSort($event, sortColumns.cost)">Cost</a></th>
                 <th></th>
             </tr>
         </thead>
         <tbody>
             <tr 
-                v-for="plugin in plugins" 
+                v-for="plugin in pluginsSorted" 
                 :key="plugin.id"
                 :class="{[`row-${plugin.category?.color}`]: plugin.category}"
             >
@@ -26,7 +26,7 @@
                 <td><a :href="plugin.group.url">{{plugin.group.name}}</a></td>
                 <td><a :href="plugin.category.url" v-if="plugin.category">{{plugin.category.name}}</a></td>
                 <td>{{ plugin.acquisition_date }}</td>
-                <td>{{ plugin.cost }}</td>
+                <td>${{ plugin.cost }}</td>
                 <td><span><a :href="plugin.edit_url" class="btn btn-primary">Edit</a></span></td>
             </tr>
         </tbody>
@@ -40,6 +40,14 @@
 <script>
 import { fetchJson } from 'umbrella-common-js/ajax.js';
 import LoadingAnimation from 'umbrella-common-js/vue/components/loading-animation.vue';
+
+const QUERY_PARAM_SORT_COLUMN = 'sort';
+const QUERY_PARAM_SORT_DIRICTION = 'direction';
+
+const SORT_DIRECTIONS = {
+    asc: 'asc',
+    desc: 'desc',
+};
 
 export default {
     props: {
@@ -58,6 +66,12 @@ export default {
         const categoriesPromise = fetchJson('/api/categories').then(categories => this.categoriesMap = categories);
 
         Promise.all([pluginsPromise, makersPromise, groupsPromise, categoriesPromise]).then(() => this.isInitialLoadComplete = true);
+
+        const params = new URLSearchParams(window.location.search);
+        const paramSortDirection = params.get(QUERY_PARAM_SORT_DIRICTION);
+        const paramSortColumn = params.get(QUERY_PARAM_SORT_COLUMN);
+        this.sortDirection = paramSortDirection in SORT_DIRECTIONS ? paramSortDirection : SORT_DIRECTIONS.asc;
+        this.sortColumn = paramSortColumn in this.sortColumns ? paramSortColumn : this.sortColumns.category;
     },
     data(){
         return {
@@ -66,6 +80,8 @@ export default {
             categoriesMap: {},
             groupsMap: {},
             makersMap: {},
+            sortColumn: '',
+            sortDirection: '',
         };
     },
     computed: {
@@ -80,18 +96,57 @@ export default {
                     group,
                     category,
                     sort: {
-                        maker: maker.sort,
-                        group: group.sort,
-                        category: category?.sort,
-                        name: plugin.name,
-                        cost: plugin.cost,
-                        acquisition_date: plugin.acquisition_date,
+                        [this.sortColumns.maker]: maker.sort,
+                        [this.sortColumns.group]: group.sort,
+                        [this.sortColumns.category]: category?.sort,
+                        [this.sortColumns.name]: plugin.name,
+                        [this.sortColumns.cost]: plugin.cost,
+                        [this.sortColumns.date]: plugin.acquisition_date,
                     },
                 };
             });
         },
+        pluginsSorted(){
+            const stringCompare = (a, b) => a.sort[this.sortColumn].localeCompare(b.sort[this.sortColumn]);
+            const numberCompare = (a, b) => a.sort[this.sortColumn] - b.sort[this.sortColumn];
+
+            const compareFunc = this.sortColumn === this.sortColumns.name || this.sortColumn === this.sortColumns.date ? stringCompare : numberCompare;
+
+            if(this.sortDirection === SORT_DIRECTIONS.asc){
+                return this.plugins.slice().sort(compareFunc);
+            }
+
+            return this.plugins.slice().sort((a, b) => compareFunc(b, a));
+        },
+        sortColumns(){
+            return {
+                maker: 'maker',
+                group: 'group',
+                category: 'category',
+                name: 'name',
+                cost: 'cost',
+                date: 'date',
+            };
+        },
     },
     methods: {
+        updateSort(event, column){
+            event.preventDefault();
+
+            if(this.sortColumn === column){
+                this.sortDirection = this.sortDirection === SORT_DIRECTIONS.asc ? SORT_DIRECTIONS.desc : SORT_DIRECTIONS.asc;
+            }
+            else {
+                this.sortColumn = column;
+                this.sortDirection = SORT_DIRECTIONS.asc;
+            }
+
+            const searchParams = new URLSearchParams(window.location.search)
+            searchParams.set(QUERY_PARAM_SORT_COLUMN, this.sortColumn);
+            searchParams.set(QUERY_PARAM_SORT_DIRICTION, this.sortDirection);
+            const newRelativePathQuery = `${window.location.pathname}?${searchParams.toString()}`;
+            history.replaceState(null, '', newRelativePathQuery);
+        },
     }
 };
 </script>
