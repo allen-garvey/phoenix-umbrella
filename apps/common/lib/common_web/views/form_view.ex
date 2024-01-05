12b9f3972dc40d4cabd188dd053e39abc59201a0
Allen Garvey <allengarvey@hotmail.com>
2024-01-05 09:53:56 -0500

    Common: Add common delete button

--- apps/common/lib/common_web/views/form_view.ex
+++ apps/common/lib/common_web/views/form_view.ex
@@ -1,11 +1,27 @@
 defmodule CommonWeb.FormView do
     use CommonWeb, :view
+
+    @doc """
+    Creates a button to trigger the hidden delete form
+    """
+    def delete_button(opts \\ []) do
+        content_tag(:button, "Delete", [class: Keyword.get(opts, :class, "btn btn-danger"), data_button: "delete", type: "button"])
+    end
+
+    def delete_button_with_form(conn, delete_path, opts \\ []) do
+        hidden_delete_form(conn, delete_path, fn _f -> delete_button(opts) end)
+    end
+
     
     @doc """
     Creates a hidden form to enable item deletion
     """
     def hidden_delete_form(conn, delete_path) do
-        form_for(conn, delete_path, [method: "delete", data_form: "delete"])
+        hidden_delete_form(conn, delete_path, fn _f -> nil end)
+    end
+
+    def hidden_delete_form(conn, delete_path, form_func) when is_function(form_func, 1) do
+        form_for(conn, delete_path, [method: "delete", data_form: "delete", class: "hidden-delete-form"], form_func)
     end
   
   end
