f3dae97ae50b2d319353bf9ec5913dbc7c3515f2
Allen Garvey <allengarvey@hotmail.com>
2024-01-12 19:17:45 -0500

    Booklist: Change title splitting to happen automatically instead of requiring a button

--- apps/booklist/assets/js/forms.js
+++ apps/booklist/assets/js/forms.js
@@ -1,19 +1,23 @@
-export const initializeTitleSplitButton = () => {
-    const button = document.querySelector('[data-button="split-title"]');
-    if (!button) {
+export const initializeTitleInputSplit = () => {
+    const titleEl = document.getElementById('book_title');
+    if (!titleEl) {
         return;
     }
 
-    button.onclick = () => {
-        const titleEl = document.getElementById('book_title');
-        const subtitleEl = document.getElementById('book_subtitle');
+    const subtitleEl = document.getElementById('book_subtitle');
+    let previousTitle = '';
 
-        const titleSplit = titleEl.value.split(':');
-        const title = titleSplit[0];
+    titleEl.onblur = (e) => {
+        const titleRaw = e.target.value;
+        if (!previousTitle) {
+            const titleSplit = titleRaw.split(':');
+            const title = titleSplit[0];
 
-        titleEl.value = title;
-        if (titleSplit.length > 1) {
-            subtitleEl.value = titleSplit[1].replace(/^\s+/, '');
+            titleEl.value = title;
+            if (titleSplit.length > 1) {
+                subtitleEl.value = titleSplit[1].replace(/^\s+/, '');
+            }
         }
+        previousTitle = titleRaw;
     };
 };
