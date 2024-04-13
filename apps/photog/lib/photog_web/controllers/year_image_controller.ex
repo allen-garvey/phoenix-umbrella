efa338b96d2e8cb2a922551f51bc41e88a36e934
Allen Garvey <allengarvey@hotmail.com>
2024-04-13 10:22:24 -0400

    Photog: Remove unneeded controller / view logic for year images

--- apps/photog/lib/photog_web/controllers/year_image_controller.ex
+++ apps/photog/lib/photog_web/controllers/year_image_controller.ex
@@ -6,29 +6,12 @@ defmodule PhotogWeb.YearImageController do
 
   action_fallback PhotogWeb.FallbackController
 
-  def index(conn, _params) do
-    year_images = Api.list_year_images()
-    render(conn, "index.json", year_images: year_images)
-  end
-
   def create(conn, %{"year_image" => year_image_params}) do
-    with {:ok, %YearImage{} = year_image} <- Api.create_year_image(year_image_params) do
+    with {:ok, %YearImage{} = _year_image} <- Api.create_year_image(year_image_params) do
       conn
       |> put_status(:created)
-      |> render("show.json", year_image: year_image)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    year_image = Api.get_year_image!(id)
-    render(conn, "show.json", year_image: year_image)
-  end
-
-  def update(conn, %{"id" => id, "year_image" => year_image_params}) do
-    year_image = Api.get_year_image!(id)
-
-    with {:ok, %YearImage{} = year_image} <- Api.update_year_image(year_image, year_image_params) do
-      render(conn, "show.json", year_image: year_image)
+      |> put_view(CommonWeb.ApiGenericView)
+      |> render("ok.json", message: "ok")
     end
   end
 
--- apps/photog/lib/photog_web/views/year_image_view.ex
+++ apps/photog/lib/photog_web/views/year_image_view.ex
@@ -1,19 +1,3 @@
 defmodule PhotogWeb.YearImageView do
   use PhotogWeb, :view
-  alias PhotogWeb.YearImageView
-
-  def render("index.json", %{year_images: year_images}) do
-    %{data: render_many(year_images, YearImageView, "year_image.json")}
-  end
-
-  def render("show.json", %{year_image: year_image}) do
-    %{data: render_one(year_image, YearImageView, "year_image.json")}
-  end
-
-  def render("year_image.json", %{year_image: year_image}) do
-    %{
-      id: year_image.id,
-      year: year_image.year
-    }
-  end
 end
