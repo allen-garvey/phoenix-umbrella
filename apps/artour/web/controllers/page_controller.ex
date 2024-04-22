5852952ab0886e8e58073f3991e6b6ba893f753a
Allen Garvey <allengarvey@hotmail.com>
2024-04-22 16:34:09 -0400

    Artour: Fix exception when there are no posts

--- apps/artour/web/controllers/page_controller.ex
+++ apps/artour/web/controllers/page_controller.ex
@@ -8,8 +8,13 @@ defmodule Artour.PageController do
   """
   def index(conn, _params) do
     posts = Public.posts_for_homepage()
+
+    facebook_image = case Enum.fetch(posts, 0) do
+      {:ok, post} -> post.cover_image
+      _ -> nil
+    end
     
-    render(conn, "index.html", posts: posts, main_container_class: "", javascript: true, facebook_image: Enum.fetch!(posts, 0).cover_image)
+    render(conn, "index.html", posts: posts, main_container_class: "", javascript: true, facebook_image: facebook_image)
   end
 
   @doc """
