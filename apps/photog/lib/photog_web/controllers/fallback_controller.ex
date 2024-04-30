8b182ed0b863a15c6ad9932227216d38c1a4948a
Allen Garvey <allengarvey@hotmail.com>
2024-04-29 22:45:08 -0400

    Photog: Fix warning in fallback controller

--- apps/photog/lib/photog_web/controllers/fallback_controller.ex
+++ apps/photog/lib/photog_web/controllers/fallback_controller.ex
@@ -9,12 +9,14 @@ defmodule PhotogWeb.FallbackController do
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
     conn
     |> put_status(:unprocessable_entity)
-    |> render(CommonWeb.ChangesetView, "error.json", changeset: changeset)
+    |> put_view(CommonWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
   end
 
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
-    |> render(PhotogWeb.ErrorView, :"404")
+    |> put_view(PhotogWeb.ErrorView)
+    |> render(:"404")
   end
 end
