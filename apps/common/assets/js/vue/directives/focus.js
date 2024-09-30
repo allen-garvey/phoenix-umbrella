4f204aab17c46e392c80d3d3a445ab8637cff97c
Allen Garvey <allengarvey@hotmail.com>
2024-09-30 16:17:42 -0400

    Common: Update v-focus directive to allow it to be conditionally disabled

--- apps/common/assets/js/vue/directives/focus.js
+++ apps/common/assets/js/vue/directives/focus.js
@@ -1,9 +1,11 @@
 //from https://stackoverflow.com/questions/34941829/setting-focus-of-an-input-element-in-vue-js
 //usage: <input v-focus>
 
-export default{
+export default {
     name: 'focus',
-    mounted(el){
-        el.focus();
-    }
+    mounted(el, binding) {
+        if (binding.value !== false) {
+            el.focus();
+        }
+    },
 };
