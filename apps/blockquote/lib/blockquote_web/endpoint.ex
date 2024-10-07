62f40fe161f26b4137af1bc301442829060a1cb6
Allen Garvey <allengarvey@hotmail.com>
2024-10-07 11:28:46 -0400

    Blockquote, Photog, Seren: Remove deprecated init function to dynamically get port from endpoint.ex

--- apps/blockquote/lib/blockquote_web/endpoint.ex
+++ apps/blockquote/lib/blockquote_web/endpoint.ex
@@ -34,19 +34,4 @@ defmodule BlockquoteWeb.Endpoint do
   plug Plug.Session, Common.Endpoint.session_options()
 
   plug BlockquoteWeb.Router
-
-  @doc """
-  Callback invoked for dynamically configuring the endpoint.
-
-  It receives the endpoint configuration and checks if
-  configuration should be loaded from the system environment.
-  """
-  def init(_key, config) do
-    if config[:load_from_system_env] do
-      port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
-      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
-    else
-      {:ok, config}
-    end
-  end
 end
