2c237025847c9ce334172e6434dafa84f54a1b42
Allen Garvey <allengarvey@hotmail.com>
2024-10-08 12:22:04 -0400

    Habits, Pluginista: Remove unused live layouts and live flashes

--- apps/habits/lib/habits_web/router.ex
+++ apps/habits/lib/habits_web/router.ex
@@ -4,7 +4,7 @@ defmodule HabitsWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_live_flash
+    plug :fetch_flash
     plug :put_root_layout, {HabitsWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
--- apps/pluginista/lib/pluginista_web/router.ex
+++ apps/pluginista/lib/pluginista_web/router.ex
@@ -4,7 +4,7 @@ defmodule PluginistaWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_live_flash
+    plug :fetch_flash
     plug :put_root_layout, {PluginistaWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
