4fc3109dd3248f0e93078d8f8b912fa56f51ae11
Allen Garvey <allengarvey@hotmail.com>
2024-10-13 12:06:40 -0400

    Grenadier: Remove common error_tag auto import, as it is unused

--- apps/grenadier/lib/grenadier_web.ex
+++ apps/grenadier/lib/grenadier_web.ex
@@ -40,7 +40,6 @@ defmodule GrenadierWeb do
       import Phoenix.HTML.Form
       use PhoenixHTMLHelpers
 
-      import Common.ViewHelpers.Form, only: [error_tag: 2]
       alias GrenadierWeb.Router.Helpers, as: Routes
     end
   end
