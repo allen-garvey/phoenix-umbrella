243885cd294f211c1ea045d6c6475fc8ebf0e991
Allen Garvey <allengarvey@hotmail.com>
2024-10-15 12:24:56 -0400

    Habits: Remove API to get activities between specific dates, as it is no longer used

--- apps/habits/lib/habits_web/controllers/api_activity_controller.ex
+++ apps/habits/lib/habits_web/controllers/api_activity_controller.ex
@@ -2,19 +2,6 @@ defmodule HabitsWeb.ApiActivityController do
     use HabitsWeb, :controller
   
     alias Habits.Api
-  
-    def index(conn, %{"from" => from, "to" => to}) do
-      case { Date.from_iso8601(from), Date.from_iso8601(to) } do
-        {{:ok, from_date}, {:ok, to_date}} ->
-          activities = Api.list_activities(from_date, to_date)
-          render(conn, "index_from_to.json", from: from, to: to, activities: activities)
-        {{_, from_error}, {_, to_error}} ->
-          conn 
-          |> put_view(CommonWeb.ApiGenericView) 
-          |> put_status(:bad_request) 
-          |> render("error.json", message: "From date #{from_error}, to date #{to_error}")
-      end
-    end
 
     def index(conn, %{"year" => year, "month" => month}) do
       with {:ok, date} <- Habits.Date.date_from(year, month) do
