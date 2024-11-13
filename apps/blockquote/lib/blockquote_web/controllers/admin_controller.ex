d8fdb695edb503c8271b314bf2aea76541ea3211
Allen Garvey <allengarvey@hotmail.com>
2024-11-13 12:51:07 -0500

    Blockquote: Refactor admin page to not use reflection to create links

--- apps/blockquote/lib/blockquote_web/controllers/admin_controller.ex
+++ apps/blockquote/lib/blockquote_web/controllers/admin_controller.ex
@@ -2,7 +2,15 @@ defmodule BlockquoteWeb.AdminController do
   use BlockquoteWeb, :controller
   
   def index(conn, _params) do
-    fields = ["author", "category", "daily quote", "quote", "parent source", "source", "source type"]
+    fields = [
+      {"Authors", &author_path/2},
+      {"Categories", &category_path/2},
+      {"Daily Quotes", &daily_quote_path/2},
+      {"Quotes", &quote_path/2},
+      {"Parent Sources", &parent_source_path/2},
+      {"Sources", &source_path/2},
+      {"Source Types", &source_type_path/2},
+    ]
     render(conn, "index.html", fields: fields)
   end
 end
