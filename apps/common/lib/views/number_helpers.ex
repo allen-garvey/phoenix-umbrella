37f2f59d62fc9ade488b4277209d9ae0bc7f2387
Allen Garvey <allen.garvey@gmail.com>
2024-12-12 11:58:38 -0500

    Common: Add number helper to cast string to integer or default value

--- apps/common/lib/views/number_helpers.ex
+++ apps/common/lib/views/number_helpers.ex
@@ -1,14 +1,32 @@
 defmodule Common.NumberHelpers do
+  @doc """
+  Converts decimal type float and rounds to 2 decimal places
+  """
+  def round_decimal(nil) do
+    0
+  end
+
+  def round_decimal(decimal) do
+    decimal
+    |> Decimal.to_float()
+    |> Float.round(2)
+  end
 
   @doc """
-	Converts decimal type float and rounds to 2 decimal places
-	"""
-	def round_decimal(nil) do
-		0
-	end
-	def round_decimal(decimal) do
-		decimal
-		  |> Decimal.to_float
-		  |> Float.round(2)
-	end
+  Converts string to integer. Returns default value on failure
+  """
+  def string_to_integer(s, default) when is_binary(s) and is_integer(default) do
+    case Integer.parse(s) do
+      {number, _} -> number
+      :error -> default
+    end
+  end
+
+  @doc """
+  Converts string to integer greater than or equal to 0. Returns default value on failure
+  """
+  def string_to_positive_integer(s, default) when is_binary(s) and is_integer(default) do
+    string_to_integer(s, default)
+    |> max(0)
+  end
 end
