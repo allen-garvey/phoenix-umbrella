04fc19e6c447b6c90fe39eb15df51f65c1bc9c77
Allen Garvey <allen.garvey@gmail.com>
2024-12-21 10:59:15 -0500

    Habits: Refactor date functions in API to use built-in Elixir date functions

--- apps/habits/lib/habits/date.ex
+++ apps/habits/lib/habits/date.ex
@@ -1,22 +1,12 @@
 defmodule Habits.Date do
-    def date_from(year, month) when is_binary(year) and is_binary(month) do
-        case {Integer.parse(year), Integer.parse(month)} do
-            {{year_int, _}, {month_int, _}} -> Date.new(year_int, month_int, 1)
-            _ -> {:error, :non_number_year_or_month}
-        end
+  def date_from(year, month) when is_binary(year) and is_binary(month) do
+    case {Integer.parse(year), Integer.parse(month)} do
+      {{year_int, _}, {month_int, _}} -> Date.new(year_int, month_int, 1)
+      _ -> {:error, :non_number_year_or_month}
     end
+  end
 
-    def has_same_month_and_year?(date1, date2) do
-        date1.year == date2.year and date1.month == date2.month
-    end
-
-    def sunday_before(date) do
-        day_of_week = Date.day_of_week(date, :sunday)
-        Date.add(date, 1 - day_of_week)
-    end
-
-    def saturday_after(date) do
-        day_of_week = Date.day_of_week(date, :sunday)
-        Date.add(date, 7 - day_of_week)
-    end
+  def has_same_month_and_year?(date1, date2) do
+    date1.year == date2.year and date1.month == date2.month
+  end
 end
--- apps/habits/lib/habits_web/controllers/api_activity_controller.ex
+++ apps/habits/lib/habits_web/controllers/api_activity_controller.ex
@@ -1,28 +1,33 @@
 defmodule HabitsWeb.ApiActivityController do
-    use HabitsWeb, :controller
-  
-    alias Habits.Api
+  use HabitsWeb, :controller
 
-    def index(conn, %{"year" => year, "month" => month}) do
-      with {:ok, date} <- Habits.Date.date_from(year, month) do
-        current_date = Date.utc_today()
+  alias Habits.Api
 
-        from_date = Habits.Date.sunday_before(date)
-        to_date = case Habits.Date.has_same_month_and_year?(date, current_date) do
-          true -> Habits.Date.saturday_after(current_date)
-          false -> Date.end_of_month(date) |> Habits.Date.saturday_after()
+  def index(conn, %{"year" => year, "month" => month}) do
+    with {:ok, date} <- Habits.Date.date_from(year, month) do
+      current_date = Common.ModelHelpers.Date.today()
+
+      from_date = Date.beginning_of_week(date, :sunday)
+
+      to_date =
+        case Habits.Date.has_same_month_and_year?(date, current_date) do
+          true -> Date.end_of_week(current_date, :sunday)
+          false -> Date.end_of_month(date) |> Date.end_of_week(:sunday)
         end
-        
-        activities = Api.list_activities(from_date, to_date)
-        
-        render(conn, "index_from_to.json", from: Date.to_iso8601(from_date), to: Date.to_iso8601(to_date), activities: activities)
-      else
-        {:error, message} -> 
-          conn 
-            |> put_view(CommonWeb.ApiGenericView) 
-            |> put_status(:bad_request) 
-            |> render("error.json", message: "#{message}")
-      end
+
+      activities = Api.list_activities(from_date, to_date)
+
+      render(conn, "index_from_to.json",
+        from: Date.to_iso8601(from_date),
+        to: Date.to_iso8601(to_date),
+        activities: activities
+      )
+    else
+      {:error, message} ->
+        conn
+        |> put_view(CommonWeb.ApiGenericView)
+        |> put_status(:bad_request)
+        |> render("error.json", message: "#{message}")
     end
+  end
 end
-  
