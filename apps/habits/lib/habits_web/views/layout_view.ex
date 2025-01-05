d186292d77e50fda17d61784d80f78e4e7bbb9bd
Allen Garvey <allen.garvey@gmail.com>
2025-01-05 11:24:07 -0500

    Habits: Refactor padding logic in root layout to happen in layout view instead of directly in template

--- apps/habits/lib/habits_web/views/layout_view.ex
+++ apps/habits/lib/habits_web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule HabitsWeb.LayoutView do
   use HabitsWeb, :view
+
+  def main_padding(disable_main_padding) do
+    case disable_main_padding do
+      true -> ""
+      _ -> "main-padding"
+    end
+  end
 end
