a2efd9edcad08eeb155f62d053cbf817df3900e4
Allen Garvey <allen.garvey@gmail.com>
2025-04-29 10:10:31 -0400

    Photog: Add ability to favorite albums on tags show pages

--- apps/photog/assets/js/routes/tags.js
+++ apps/photog/assets/js/routes/tags.js
@@ -1,4 +1,5 @@
 import { sortAlbumsCallback } from '../route-helpers/sorting.js';
+import { API_URL_BASE } from '../request-helpers.js';
 
 import ThumbnailList from '../components/thumbnail-list.vue';
 import TagForm from '../components/tag-form.vue';
@@ -91,7 +92,23 @@ export default () => [
                     name: 'tagSlideshow',
                     params: { tag_id: route.params.id },
                 },
+                updateItemFavorite(sendJson, item) {
+                    const id = item.id;
+
+                    const newValue = !item.is_favorite;
+                    item.is_favorite = newValue;
+
+                    const apiUrl = `${API_URL_BASE}/albums/${id}`;
+
+                    return sendJson(apiUrl, 'PATCH', {
+                        album: {
+                            id,
+                            is_favorite: newValue,
+                        },
+                    });
+                },
             };
+
             return props;
         },
     },
