fc57f7bcb6c7a513c64c196ecdca903cd022453b
Allen Garvey <allen.garvey@gmail.com>
2025-06-08 10:30:53 -0400

    Habits: Add delete button to activity form pages

--- apps/habits/assets/js/index.js
+++ apps/habits/assets/js/index.js
@@ -1,5 +1,6 @@
 import { instantiateVue } from 'umbrella-common-js/vue/instantiate-vue.js';
 import { initializeYesterdayButton } from 'umbrella-common-js/date-input.js';
+import { initializeFormDeleteButton } from 'umbrella-common-js/form-delete-button.js';
 import { initializeActivityTitleSuggestions } from './activity-title-suggestions';
 
 import ActivityCalendar from './vue/activity-calendar.vue';
@@ -7,15 +8,6 @@ import ActivityCalendar from './vue/activity-calendar.vue';
 import css from '../css/app.scss';
 
 initializeYesterdayButton();
+initializeFormDeleteButton();
 instantiateVue('activity-calendar', ActivityCalendar, ['newActivityUrl']);
 initializeActivityTitleSuggestions();
-
-document.querySelectorAll('[data-button="delete"]').forEach(button => {
-    button.onclick = e => {
-        if (!confirm('Are you sure you want to delete?')) {
-            e.preventDefault();
-            return false;
-        }
-        button.closest('form').submit();
-    };
-});
