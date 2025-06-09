5dbdf8580e1f6bddf7b3a26ceb75e42a90054069
Allen Garvey <allen.garvey@gmail.com>
2025-06-09 10:48:05 -0400

    Habits: Turn urls into links in activity descriptions

--- apps/habits/lib/habits_web/views/activity_view.ex
+++ apps/habits/lib/habits_web/views/activity_view.ex
@@ -1,4 +1,20 @@
 defmodule HabitsWeb.ActivityView do
   alias HabitsWeb.CategoryView
   use HabitsWeb, :view
+
+  def linkify_description(description) when is_binary(description) do
+    url_regex = ~r/\bhttps?:\/\/\S+/
+
+    Regex.split(url_regex, description, include_captures: true)
+    |> Enum.map(fn word ->
+      case Regex.match?(url_regex, word) do
+        true -> link(word, to: word, target: "_blank", rel: "noopener noreferrer")
+        false -> word
+      end
+    end)
+  end
+
+  def linkify_description(description) do
+    description
+  end
 end
