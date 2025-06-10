15c9df2f4e3d57b280ce4966f3cf564818ff9cc0
Allen Garvey <allen.garvey@gmail.com>
2025-06-10 11:27:04 -0400

    Habits: Optimize linkify activity description regex

--- apps/habits/lib/habits_web/views/activity_view.ex
+++ apps/habits/lib/habits_web/views/activity_view.ex
@@ -5,9 +5,12 @@ defmodule HabitsWeb.ActivityView do
   def linkify_description(description) when is_binary(description) do
     url_regex = ~r/\bhttps?:\/\/\S+/
 
+    # use modified regex in split to increase performance, since we don't need to search the whole string and can just match from the start
+    url_split_regex = ~r/^https?:\/\/\S+/
+
     Regex.split(url_regex, description, include_captures: true)
     |> Enum.map(fn word ->
-      case Regex.match?(url_regex, word) do
+      case Regex.match?(url_split_regex, word) do
         true -> link(word, to: word, target: "_blank", rel: "noopener noreferrer")
         false -> word
       end
