d495dd5fd498ed736f7c2c41f9f38542e2930e3c
Allen Garvey <allen.garvey@gmail.com>
2026-07-13 18:18:09 -0400

    Supersearch: Add search results page

--- apps/grenadier/lib/supersearch/supersearch_web/controllers/page_controller.ex
+++ apps/grenadier/lib/supersearch/supersearch_web/controllers/page_controller.ex
@@ -1,7 +1,14 @@
 defmodule SupersearchWeb.PageController do
   use GrenadierWeb, :controller
+  alias Supersearch.Admin
 
   plug(:put_view, html: SupersearchWeb.PageView)
+
+  def index(conn, %{"q" => query}) do
+    engines = Admin.list_engines()
+
+    render(conn, "search_results.html", query: query, engines: engines)
+  end
 
   def index(conn, _params) do
     render(conn, "index.html")
--- apps/grenadier/lib/supersearch/supersearch_web/views/engine_view.ex
+++ apps/grenadier/lib/supersearch/supersearch_web/views/engine_view.ex
@@ -1,3 +1,8 @@
 defmodule SupersearchWeb.EngineView do
   use SupersearchWeb, :view
+  alias Supersearch.Admin.Engine
+
+  def to_query_url(%Engine{} = engine, query) when is_binary(query) do
+    String.replace(engine.url, "{$cleanedQuery}", URI.encode(query))
+  end
 end
