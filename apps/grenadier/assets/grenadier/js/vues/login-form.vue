33f0272eaef0e56de823848558b41cdd27df5d96
Allen Garvey <allen.garvey@gmail.com>
2026-07-17 16:13:08 -0400

    Grenadier: Remove unused JS import

--- apps/grenadier/assets/grenadier/js/vues/login-form.vue
+++ apps/grenadier/assets/grenadier/js/vues/login-form.vue
@@ -1,19 +1,39 @@
 <template>
     <div>
         <h1 @click="showForm()" v-if="!shouldShowForm">Under Construction</h1>
-        <form class="login-form" method="POST" :action="loginUrl" v-if="shouldShowForm">
+        <form
+            class="login-form"
+            method="POST"
+            :action="loginUrl"
+            v-if="shouldShowForm"
+        >
             <input type="hidden" name="_csrf_token" :value="csrfToken" />
-            <input name="_utf8" type="hidden" value="✓"/>
-            <input name="redirect" type="hidden" :value="redirect" v-if="redirect">
-            <input type="text" placeholder="Identifier" name="username" v-model="username" v-focus />
-            <input type="password" placeholder="Proof" name="password" v-model="password" />
+            <input name="_utf8" type="hidden" value="✓" />
+            <input
+                name="redirect"
+                type="hidden"
+                :value="redirect"
+                v-if="redirect"
+            />
+            <input
+                type="text"
+                placeholder="Identifier"
+                name="username"
+                v-model="username"
+                v-focus
+            />
+            <input
+                type="password"
+                placeholder="Proof"
+                name="password"
+                v-model="password"
+            />
             <button type="submit">Login</button>
         </form>
     </div>
 </template>
 
 <script>
-import { sendJson } from 'umbrella-common-js/ajax.js';
 import focus from 'umbrella-common-js/vue/directives/focus.js';
 
 export default {
@@ -30,7 +50,7 @@ export default {
     directives: {
         focus,
     },
-    data(){
+    data() {
         return {
             shouldShowForm: false,
             username: '',
@@ -41,13 +61,12 @@ export default {
         redirect() {
             const urlParams = new URLSearchParams(window.location.search);
             return urlParams.get('redirect');
-        }
+        },
     },
     methods: {
-        showForm(){
+        showForm() {
             this.shouldShowForm = true;
         },
-    }
+    },
 };
 </script>
-
