10df929d2a5f0d4f70a4353954c7891abe1da4a0
Allen Garvey <allen.garvey@gmail.com>
2026-08-02 19:31:47 -0400

    Photog: Fix directory traversal vulnerability when serving images

--- apps/photog/lib/photog_web/controllers/static_file_controller.ex
+++ apps/photog/lib/photog_web/controllers/static_file_controller.ex
@@ -12,9 +12,18 @@ defmodule PhotogWeb.StaticFileController do
   defp serve_file(conn, source_path_env_key, path) when is_list(path) do
     case Application.fetch_env(:photog, source_path_env_key) do
       {:ok, image_dir} ->
-        conn
-        |> put_resp_header("cache-control", "private, max-age=86400")
-        |> send_file(200, Path.join([image_dir] ++ path))
+        full_path = Path.join([image_dir] ++ path) |> Path.expand()
+
+        # check for directory traversal
+        case String.starts_with?(full_path, image_dir) do
+          true ->
+            conn
+            |> put_resp_header("cache-control", "private, max-age=86400")
+            |> send_file(200, full_path)
+
+          false ->
+            send_resp(conn, 404, "File not found")
+        end
 
       _ ->
         send_resp(conn, 500, "#{source_path_env_key} not set")
