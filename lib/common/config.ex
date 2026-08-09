63486e3e4038facc4079070706a811b22e3fefa8
Allen Garvey <allen.garvey@gmail.com>
2026-08-09 17:46:32 -0400

    Common: Remove postgres config function, since it is now unused

--- lib/common/config.ex
+++ lib/common/config.ex
@@ -3,19 +3,6 @@ defmodule Umbrella.Common.Config do
     System.get_env("UMBRELLA_SECRET_KEY_BASE", "pTlqYpnNuuhdjjy+uza2Ih+G6GdE8/nYiATPOT6PEAkcWTB4qjtH0+urfiYJwF0X")
   end
 
-  def postgres_config(database_name) when is_binary(database_name) do
-    [
-      username: "postgres",
-      password: "postgres",
-      database: database_name,
-      hostname: "localhost",
-      show_sensitive_data_on_connection_error: true,
-      port: System.get_env("UMBRELLA_DB_PORT", "5432"),
-      pool_size: 10,
-      queue_target: 5000
-    ]
-  end
-
   def grenadier_port do
     6009
   end
